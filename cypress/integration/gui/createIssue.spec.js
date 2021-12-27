8385b6d4116852072543b097ca0775dd990dc20e
cassia-cristina <cassia.cristina.go@gmail.com>
2021-12-27 20:21:37 -0300

    testes api e cli

---
+++ cypress/integration/gui/createIssue.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="Cypress" />
+
+const faker = require('faker')
+
+describe('Create Issue', () => {
+    const issue = {
+        title: `issue-${faker.datatype.uuid()}`,
+        description: faker.random.words(3),
+        project: {
+            name: `project-${faker.datatype.uuid()}`,
+            description: faker.random.words(5),
+        }
+    }
+
+    beforeEach(() => {
+        cy.login()
+        cy.api_createProject(issue.project)
+    })
+
+    it('sucessfully', () => {
+        cy.gui_createIssue(issue)
+        cy.get('.issue-details')
+          .should('contain',issue.title)
+          .and('contain', issue.description)
+    });
+    
+});
---
+++ cypress/support/gui_commands.js
@@ -0,0 +1,41 @@
+/// <reference types="Cypress" />
+
+Cypress.Commands.add('login', () => {
+    cy.visit('users/sign_in')
+    cy.get("[name='user[login]']").type(Cypress.env('user_name'))
+    cy.get("[name='user[password]']").type(Cypress.env('user_password'))
+    cy.get('[data-qa-selector=sign_in_button]').click()
+})
+
+Cypress.Commands.add('logout', () => {
+    cy.get('.header-user-dropdown-toggle').click()
+    //cy.get('.sign-out-link').click()
+    cy.contains('Sign out').click()
+})
+
+Cypress.Commands.add('gui_createProject', project => {
+    cy.visit('projects/new')
+    cy.get('#project_name').type(project.name)
+    cy.get('#project_description').type(project.description)
+    cy.get("input[name='project[initialize_with_readme]']").check()
+    //cy.get("input[name='commit']").click()
+    cy.contains('Create project').click()
+})
+
+Cypress.Commands.add('gui_createIssue', issue => {
+    cy.visit(`${Cypress.env('user_name')}/${issue.project.name}/issues/new`)
+    cy.get('.qa-issuable-form-title').type(issue.title)
+    cy.get('.qa-issuable-form-description').type(issue.description)
+    cy.contains('Submit issue').click()
+})
+
+Cypress.Commands.add('gui_setLabelOnIssue', label => {
+    cy.get('.qa-edit-link-labels').click()
+    cy.contains(label.name).click()
+    cy.get('body').click()
+})
+
+Cypress.Commands.add('gui_setMilestoneOnIssue', milestone => {
+    cy.get('.block.milestone .edit-link').click()
+    cy.contains(milestone.title).click()
+})
